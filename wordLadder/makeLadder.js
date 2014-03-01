93da9732ef8841870354099f322e32a6ad4b940a
daviis <daviis@luther.edu>
2014-03-01 11:03:13 -0600

    myStack is complete

--- wordLadder/makeLadder.js
+++ wordLadder/makeLadder.js
@@ -36,5 +36,43 @@ var MyQueue = function(){
 }
 
 var MyStack = function(){
+	var arr = new Array()
+	var length = 0
+
+	this.pop = function(){
+		length -= 1
+		return arr.pop()
+	}
+	
+	this.push = function(word){
+		length += 1
+		arr.push(word)
+	}
+
+	this.length = function(){
+		return length
+	}
+
+	this.clone = function(){
+		aStack = new MyStack()
+		var tempArr = new Array()
+		for(var i=0; i<=this.length()+1; i++){
+			var word = this.pop()
+			tempArr.push(word)
+		}
+		for(var i=tempArr.length-1; i>=0; i--){
+			this.push(tempArr[i])
+			aStack.push(tempArr[i])
+		}
+		return aStack	
+	}
 
 }
+
+
+
+
+
+
+
+
