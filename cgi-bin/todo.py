3cee75d3687c8eca1c7fa9d7bbae370c1408288d
daviis <daviis@luther.edu>
2014-03-13 12:47:08 -0500

    started todo, need to branch

--- cgi-bin/todo.py
+++ cgi-bin/todo.py
@@ -1,11 +1,20 @@
-#!/usr/bin/python
+#!/home/students/daviis01/cs365/todoenv/bin/python3
 
-print "Content-type: text/html"
-print ""
+import yaml
 
-print "<h1>ToDo!</h1>"
-aFile = open("/home/students/daviis01/cs365/daviis.github.io/todo/list.txt")
-for line in aFile:
-	print "<input type=checkbox>" + line + "<br>"
-
-
+print ("Content-type: text/html")
+print ("")
+print ('''<head>
+	</head>
+	<body>''')
+try:
+	aFile = open("/home/students/daviis01/cs365/daviis.github.io/todo/list.txt")
+	aDict = yaml.load(aFile)	
+	print("<h1>ToDo!</h1>")
+	print(type(aDict))
+#	for line in aFile:
+#		print ("<input type=checkbox>" + line + "<br>")
+except IOError as er:
+	print ('''<h1>Cant open file!</h1>"''')
+	raise er
+print ('''</body>''')
