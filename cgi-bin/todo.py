d03d33b306374782de1c5e6976ad027afb9321fe
daviis <daviis@luther.edu>
2014-03-13 19:11:41 -0500

    added priority color, decoded yaml

--- cgi-bin/todo.py
+++ cgi-bin/todo.py
@@ -5,16 +5,27 @@ import yaml
 print ("Content-type: text/html")
 print ("")
 print ('''<head>
+	<link rel="stylesheet" type="text/css" href="../theme.css">
 	</head>
 	<body>''')
 try:
-	aFile = open("/home/students/daviis01/cs365/daviis.github.io/todo/list.txt")
+	aFile = open("/home/students/daviis01/cs365/daviis.github.io/todo/todo.dat")
 	aDict = yaml.load(aFile)	
 	print("<h1>ToDo!</h1>")
-	print(type(aDict))
-#	for line in aFile:
-#		print ("<input type=checkbox>" + line + "<br>")
+	for key in aDict:
+		print("<h3>"+key+"</h3>")
+		if(aDict[key] != None):
+			for todoItem in aDict[key]:
+				try:
+					itemDict = aDict[key][todoItem]
+					print("<div class="+ str(itemDict['priority']) +">")
+				except KeyError:
+					print("<div>")
+					continue					
+				finally:
+					print("<input type=checkbox name="+str(todoItem)+">"+str(itemDict['task'])+"</input>")
+					print("<br>")
+					print("</div>")	
 except IOError as er:
 	print ('''<h1>Cant open file!</h1>"''')
-	raise er
 print ('''</body>''')
